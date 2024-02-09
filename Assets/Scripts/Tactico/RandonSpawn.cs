3620a3151ce34b58cdd5cc1081eef8e200b4a4c2
LAPURACHULETA <101416312+LAPURACHULETA@users.noreply.github.com>
2024-02-09 13:01:58 -0600

    lava obsidiana
    spawner modificado

--- Assets/Scripts/Tactico/RandonSpawn.cs
+++ Assets/Scripts/Tactico/RandonSpawn.cs
@@ -8,23 +8,33 @@ public class RandonSpawn : MonoBehaviour
     private GameObject roca;
     [SerializeField]
     private GameObject diamante;
+    [SerializeField]
+    private GameObject obsidiana;
+    [SerializeField]
+    private GameObject lava;
 
     [SerializeField]
     private float spawnTimeRoca = 3.5f;
     [SerializeField]
     private float spawnTimeDiamante = 10f;
+    [SerializeField]
+    private float spawnTimeObsidiana = 3.5f;
+    [SerializeField]
+    private float spawnTimeLava = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnEnemy(spawnTimeRoca, roca));
         StartCoroutine(spawnEnemy(spawnTimeDiamante, diamante));
+        StartCoroutine(spawnEnemy(spawnTimeRoca, obsidiana));
+        StartCoroutine(spawnEnemy(spawnTimeDiamante, lava));
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject roca)
+    private IEnumerator spawnEnemy(float interval, GameObject minerales)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newRoca = Instantiate(roca, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, roca));
+        GameObject newMineral = Instantiate(minerales, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, minerales));
     }
 }
