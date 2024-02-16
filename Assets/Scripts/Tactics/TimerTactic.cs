ac3859c53827653dd2fc727b109a7a375fbd899d
RRouxx <pks_atlas@hotmail.com>
2024-02-16 14:33:13 -0600

    timer add

--- Assets/Scripts/Tactics/TimerTactic.cs
+++ Assets/Scripts/Tactics/TimerTactic.cs
@@ -6,25 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class TimerTactic : MonoBehaviour
 {
-    [SerializeField] float time = 60;
+    [SerializeField] float initialTime = 60;
+    float currentTime;
     public bool timeFinished = false;
     [SerializeField]
     TextMeshProUGUI txtEnd;
+    [SerializeField]
+    TextMeshProUGUI txtTimer;
 
     void Start()
     {
         timeFinished = false;
-        Invoke("FinishGame", time);
+        currentTime = initialTime;
+        UpdateTimerText();
+        StartCoroutine(StartTimer());
     }
 
-    private void Update()
+    IEnumerator StartTimer()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && timeFinished)
+        while (currentTime > 0)
         {
-            SceneManager.LoadScene(gameObject.scene.buildIndex);
-            Time.timeScale = 1;
-            txtEnd.gameObject.SetActive(false);
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+            UpdateTimerText();
         }
+
+        FinishGame();
+    }
+
+    private void UpdateTimerText()
+    {
+        txtTimer.text = "Time: " + Mathf.RoundToInt(currentTime).ToString();
     }
 
     private void FinishGame()
@@ -32,5 +44,20 @@ public class TimerTactic : MonoBehaviour
         timeFinished = true;
         txtEnd.gameObject.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && timeFinished)
+        {
+            RestartGame();
+        }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(gameObject.scene.buildIndex);
+        Time.timeScale = 1;
+        txtEnd.gameObject.SetActive(false);
     }
 }
